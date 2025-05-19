ab7c45aa3e6071419ce9f4486fc481ddb584d61d
David Zirinsky <dzirinsky@gmail.com>
2025-05-19 09:37:11 -0600

    latest changes

--- _next/static/chunks/app/layout-8ba53d6fc82e0f8d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5113:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8563,23)),Promise.resolve().then(l.t.bind(l,5299,23))},5299:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},8563:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[575,441,684,358],()=>s(5113)),_N_E=e.O()}]);
